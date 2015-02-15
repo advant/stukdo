ea40d2dfbbb30ffa1433869c09a9966337a12b93
Drew Hoang <drewhoang@Drews-MacBook-Pro.local>
2015-02-14 18:42:39 -0800

    Installed bootstrap

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,10 @@ module Stukdo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += ["fontawesome-webfont.ttf",
+                             "fontawesome-webfont.eot",
+                             "fontawesome-webfont.svg",
+                             "fontawesome-webfont.woff"]
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
